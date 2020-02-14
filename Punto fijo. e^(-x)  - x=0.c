1f1429d11e0be204fca8c6adef507b50bbbe91c8
Emiliano27ss <60231705+Emiliano27ss@users.noreply.github.com>
2020-02-14 00:59:03 -0600

    Add files via upload
    Emiliano Sanchez olivares 
    Grupo 2SV2
    Tarea 3. Punto fijo

---
+++ Punto fijo. e^(-x)  - x=0.c
@@ -0,0 +1,54 @@
+/*Alumno: Emiliano Sanchez Olivares
+Grupo: 2SV2
+Curso: Metodos Numericos
+Tarea: 3.1
+
+*/
+#include<stdio.h>
+#include<conio.h>
+#include<math.h>
+float xInicial, error;
+float raiz,e_test=0, f_xr,f_xanterir,xr,xranterior;
+int cont=0;
+float f_x(float x)
+{
+    return (exp(-x));
+}
+
+float puntofijo(float Xi, float e)
+{
+do{
+   //operaciones
+xr=f_x(Xi);
+if(xr==0){break;}
+printf("\nValor de xr:%f",xr);
+printf("\nValor de xi:%f",Xi);
+e_test=fabs(xr-Xi);
+Xi=xr;
+
+printf("\nValor del error:%f\n\n",e_test);
+cont=cont+1;
+}
+while(e_test>e);
+raiz=xr;
+printf("Numero de iteraciones:%i",cont);
+return raiz;
+}
+int main()
+{
+printf("\n\tPrograma que calcula la raiz de la funcion    2x^2 -x -5 = 0 por el metodo de punto fijo   ");
+printf("\nDigita un valor inicial para empezar a evaluar la funcion");
+printf("\nX inicial: ");
+scanf("%f",&xInicial);
+
+printf("\nDigita el error permitido: ");
+scanf("%f",&error);
+
+puntofijo(xInicial,error);
+
+printf("\nEL valor de la raiz es aproximadamente:%f",raiz);
+
+
+getch();
+return 0;
+}
